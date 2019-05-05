e2fb29303c81d0103827ed2116d25006ee1300f7
Patrick A <akolo@hotmail.com>
2019-05-05 21:11:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,20 +9,37 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri:: HTML(open (./fixtures/student-site/index.html))
+<<<<<<< HEAD
     
+=======
+>>>>>>> b716c34540e658bd6ccf3a2be7583c8de8b27939
     doc.css(".roster-cards-container")each do |post|
     student = Student.new 
     student.name = roster-cards-container.css("h4").text 
     student.location = roster-cards-container.css("p").text
     student.profile_url = roster-cards-container.css("h3").text
+<<<<<<< HEAD
     end 
   end 
+=======
+    
+  end 
+end 
+    #scraped_students = {}
+    
+    
+    
+    
+>>>>>>> b716c34540e658bd6ccf3a2be7583c8de8b27939
     
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri:: HTML(open (http://twitter.com/flatironschool))
+<<<<<<< HEAD
     
     
+=======
+>>>>>>> b716c34540e658bd6ccf3a2be7583c8de8b27939
   end
 
 end
